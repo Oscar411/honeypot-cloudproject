1db643f77a45d02528f8db4448a04a0f28200cb2
Oscar Herrera <Oscar.Herrera@csu.fullerton.edu>
2025-11-19 05:35:16 +0000

    Ingress into the EC2 with the ports, SSH & HTTP for attackers. Egress for traffic for the S3 and SNS

---
+++ network/security_group.tf
@@ -0,0 +1,31 @@
+resouce "aws_resource_group" "honeypot_sg"{
+        name        = "honeypot-sg"
+        description = "Security group for honeypot EC2 instance"
+        vpc_id      = aws_vpc.honeypot_vpc.id
+
+        ingress {
+           description = "SSH"
+           from_port   = 22
+           to_port     = 22
+           protocol    = "tcp"
+           cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        ingress {
+           description = "HTTP"
+           from_port   = 80
+           to_port     = 80
+	    protocol    = "tcp"
+           cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+           description = "Allow S3 and SNS"
+           from_port   = 443
+           to_port     = 443
+           protocol    = "tcp"
+           cidr_blocks = ["0.0.0.0/0"]
+        } 
+
+        tags = { Name = "honeypot-sg" }
+} 
